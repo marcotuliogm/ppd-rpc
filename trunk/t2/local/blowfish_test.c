bfe9e4fef768efe18c3ad3617867532419bcb828
vgkleinubing <vgkleinubing@68ae4a85-b1ec-ad7a-dcb5-dcafc6c920a5>
2010-11-29 22:49:44 +0000

    commit

--- trunk/t2/local/blowfish_test.c
+++ trunk/t2/local/blowfish_test.c
@@ -17,23 +17,24 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 #include <stdio.h>
-#include "blowfish.h"
+#include "blowfish.c"
 
-
-void main(void) {
-  unsigned long L = 1, R = 2;
-  BLOWFISH_CTX ctx;
-
-  Blowfish_Init (&ctx, (unsigned char*)"TESTKEY", 7);
-  Blowfish_Encrypt(&ctx, &L, &R);
-  printf("%08lX %08lX\n", L, R);
-  if (L == 0xDF333FD2L && R == 0x30A71BB4L)
-	  printf("Test encryption OK.\n");
-  else
-	  printf("Test encryption failed.\n");
-  Blowfish_Decrypt(&ctx, &L, &R);
-  if (L == 1 && R == 2)
-  	  printf("Test decryption OK.\n");
-  else
-	  printf("Test decryption failed.\n");
+void main(void)
+{
+	unsigned char key[] = "TESTKEY";
+	unsigned long L = 1, R = 2;
+	BLOWFISH_CTX ctx;
+	Blowfish_Init (&ctx, key, sizeof(key)-1);
+	Blowfish_Encrypt(&ctx, &L, &R);
+	printf("%08lX %08lX\n", L, R);
+	if (L == 0xDF333FD2L && R == 0x30A71BB4L)
+		printf("Test encryption OK.\n");
+	else
+		printf("Test encryption failed.\n");
+	Blowfish_Decrypt(&ctx, &L, &R);
+	if (L == 1 && R == 2)
+		printf("Test decryption OK.\n");
+	else
+		printf("Test decryption failed.\n");
 }
+
